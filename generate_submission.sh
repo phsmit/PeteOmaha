0d32ab80d256efffbad0bf4cfd4e580c3e08cc38
Peter Smit <peter@smitmail.eu>
2014-05-26 16:33:41 +0300

    WIP

--- generate_submission.sh
+++ generate_submission.sh
@@ -4,5 +4,6 @@ rm -Rf subm/
 mkdir subm
 cp *.h *.c pe/include/*.h pe/lib/*.c pe/lib/*.h subm
 cd subm
+gcc -Wall -Wextra -o test *.c
 zip subm.zip *.c *.h
 cd ..
--- peteomaha.c
+++ peteomaha.c
@@ -12,10 +12,7 @@
 /*
  * 
  */
-int main(int argc, char** argv) {
-//    puts("check 0");
-//    fflush(stdout);
-            
+int main(int argc, char** argv) {          
     size_t nbytes = 100;
     char *line;
     
