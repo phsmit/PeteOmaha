82377b445ead2745248dfc38f8c284721349a7ca
Peter Smit <peter@smitmail.eu>
2014-05-26 16:27:45 +0300

    First working version

--- generate_submission.sh
+++ generate_submission.sh
@@ -2,7 +2,7 @@
 
 rm -Rf subm/
 mkdir subm
-cp *.c pe/include/*.h pe/lib/*.c pe/lib/*.h subm
+cp *.h *.c pe/include/*.h pe/lib/*.c pe/lib/*.h subm
 cd subm
 zip subm.zip *.c *.h
 cd ..
---
+++ peteomaha.c
@@ -0,0 +1,39 @@
+/* 
+ * File:   peteomaha.c
+ * Author: psmit
+ *
+ * Created on May 26, 2014, 3:31 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "parsing.h"
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+//    puts("check 0");
+//    fflush(stdout);
+            
+    size_t nbytes = 100;
+    char *line;
+    
+    po_settings *settings;
+    po_match *match;
+    
+    line = (char *) malloc(nbytes + 1);
+    
+    int action_time = 0;
+    while(getline(&line, &nbytes, stdin))
+    {
+        action_time = po_parse_next_line(line, match, settings);
+        if (action_time) {
+            puts("check 0");
+            fflush(stdout);
+        }
+    }
+
+    return (EXIT_SUCCESS);
+}
+
