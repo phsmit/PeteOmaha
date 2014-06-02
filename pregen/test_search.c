52eb80d5d909ec2bba170a81f0d5dbc6a51c2351
Peter Smit <peter@smitmail.eu>
2014-06-02 15:44:48 +0300

    Start of startinghands.h

--- pregen/test_search.c
+++ pregen/test_search.c
@@ -8,11 +8,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "poker_defs.h"
+#include "starting_hands.h"
 /*
  * 
  */
 int main(int argc, char** argv) {
 
+    fill_uniques();
+    
+    StdDeck_CardMask hole;
+    StdDeck_CardMask empty_deck;
+    StdDeck_CardMask_RESET(empty_deck);
+    
+    
+    DECK_ENUMERATE_4_CARDS_D(StdDeck, hole, empty_deck, {
+        if(hole.cards.spades >= hole.cards.clubs &&
+                hole.cards.clubs >= hole.cards.diamonds &&
+                hole.cards.diamonds >= hole.cards.hearts) {
+            puts(StdDeck_maskString(hole));
+            //print hand
+        }
+//        
+    });
+    
     return (EXIT_SUCCESS);
 }
 
