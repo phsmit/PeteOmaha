dbf0d59d5ddf5a951b85ef131cf13e7cab367a20
Peter Smit <peter@smitmail.eu>
2014-06-03 14:25:32 +0300

    Finish prob generation

---
+++ include/probtables.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   probtables.h
+ * Author: peter
+ *
+ * Created on June 3, 2014, 2:19 PM
+ */
+
+#ifndef PROBTABLES_H
+#define	PROBTABLES_H
+
+double win_table[16432];
+double draw_table[16432];
+double loss_table[16432];
+#endif	/* PROBTABLES_H */
+
---
+++ programs/build_probs_file.c
@@ -0,0 +1,81 @@
+/* 
+ * File:   build_probs_file.c
+ * Author: peter
+ *
+ * Created on June 3, 2014, 1:59 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "starting_hands.h"
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    fill_uniques();
+    double wins[NUM_UNIQUES];
+    double draws[NUM_UNIQUES];
+    double losses[NUM_UNIQUES];
+    
+    size_t nbytes = 100;
+    char *line;
+    line = (char *) malloc(nbytes + 1);
+    
+    StdDeck_CardMask hand, card;
+    
+    int i, card_i, index;
+    
+    while(getline(&line, &nbytes, stdin)  > 0)
+    {
+        StdDeck_CardMask_RESET(hand);
+        for (i = 0; i < 4; ++i) {
+            StdDeck_stringToCard(line+i*3, &card_i); 
+            card = StdDeck_MASK(card_i);
+            StdDeck_CardMask_OR(hand, hand, card);
+        }
+        
+        line[strlen(line) - 1] = '\0';
+        double win = atof(strtok(line+12, " "));
+        double draw = atof(strtok(NULL, " "));
+        double loss = atof(strtok(NULL, " "));
+        
+        get_unique(&hand);
+        
+        index = find_unique_index(hand);
+        wins[index] = win;
+        draws[index] = draw;
+        losses[index] = loss;
+    }
+    puts("#include \"probtables.h\"");
+    printf("double win_table[%d] = { \n", NUM_UNIQUES);
+    for (i = 0; i < NUM_UNIQUES; ++i) {
+        printf("%.2f", wins[i]);
+        if (i < NUM_UNIQUES -1) {
+            printf(",\n");
+        }
+    }
+    printf("};\n\n");
+    
+    printf("double draw_table[%d] = { \n", NUM_UNIQUES);
+    for (i = 0; i < NUM_UNIQUES; ++i) {
+        printf("%.2f", draws[i]);
+        if (i < NUM_UNIQUES -1) {
+            printf(",\n");
+        }
+    }
+    printf("};\n\n");
+    
+    printf("double loss_table[%d] = { \n", NUM_UNIQUES);
+    for (i = 0; i < NUM_UNIQUES; ++i) {
+        printf("%.2f", losses[i]);
+        if (i < NUM_UNIQUES -1) {
+            printf(",\n");
+        }
+    }
+    printf("};\n\n");
+    
+    return (EXIT_SUCCESS);
+}
+
