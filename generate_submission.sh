b54ba66cd20e11737a9a90dc3d4ecb9994038411
Peter Smit <peter@smitmail.eu>
2014-06-04 15:14:39 +0300

    Fix submission script

--- generate_submission.sh
+++ generate_submission.sh
@@ -2,7 +2,7 @@
 
 rm -Rf subm/
 mkdir subm
-cp include/*.h lib/*.c pe/include/*.h pe/lib/*.c pe/lib/*.h programs/peteomaha.c subm
+cp include/*.h lib/*.c vendor/*/include/*.h vendor/*/lib/*.c vendor/*/lib/*.h programs/peteomaha.c subm
 cd subm
 gcc -Wall -Wextra -o test *.c
 zip subm.zip *.c *.h
